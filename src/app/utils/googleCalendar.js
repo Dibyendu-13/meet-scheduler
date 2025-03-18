7ac07b6c688f42fa1ac85590fc23451e0a11c8be
Dibyendu Bar <82048885+Dibyendu-13@users.noreply.github.com>
2025-03-18 13:43:03 +0530

    Update googleCalendar.js

--- src/app/utils/googleCalendar.js
+++ src/app/utils/googleCalendar.js
@@ -1,15 +1,12 @@
 import { google } from "googleapis";
-import fs from "fs";
-import path from "path";
 
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events"];
-
-// Load Service Account Credentials
-const KEY_FILE_PATH = path.join(process.cwd(), "config/service-account.json");
-const credentials = JSON.parse(fs.readFileSync(KEY_FILE_PATH, "utf8"));
-
-// Google Calendar ID
 const GOOGLE_CALENDAR_ID = process.env.GOOGLE_CALENDAR_ID;
+
+// Decode service account JSON from environment variable
+const credentials = JSON.parse(
+  Buffer.from(process.env.GOOGLE_SERVICE_ACCOUNT, "base64").toString("utf-8")
+);
 
 // Authenticate Google API with impersonation
 const auth = new google.auth.JWT(
@@ -17,8 +14,8 @@ const auth = new google.auth.JWT(
   null,
   credentials.private_key.replace(/\\n/g, "\n"),
   SCOPES,
-  GOOGLE_CALENDAR_ID 
+  GOOGLE_CALENDAR_ID
 );
 
 export const calendar = google.calendar({ version: "v3", auth });
-export { GOOGLE_CALENDAR_ID }; 
+export { GOOGLE_CALENDAR_ID };
